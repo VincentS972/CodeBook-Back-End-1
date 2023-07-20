03a285521d4fcb70c5efeb6add4e4297ccef11f3
VincentS972 <vincentsechi@gmail.com>
2023-07-19 20:07:47 -0400

    corrected profil

--- models/Profile.js
+++ models/Profile.js
@@ -1,31 +1,72 @@
-const router = require('express').Router()
-const {
-    getAllProfiles,
-    createProfile,
-    deleteProfileById,
-    updateProfileById,
-    getProfileById,
-    getProfileByEmail
-} = require('../controllers/Profile')
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
-// GET / get all Profiles
-router.get('/', getAllProfiles)
+const profileSchema = new mongoose.Schema({
+    userName: {
+      type: String,
+      required: true
+    },
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    profilePicture: {
+      type: String,
+      default: 'https://upload.wikimedia.org/wikipedia/commons/b/b5/Windows_10_Default_Profile_Picture.svg'
+    },
+    getsUpdates: {
+      type: Boolean,
+      default: false
+    },
+    password: {
+      type: String,
+      required: true
+    }
+   
+}, {
+  timestamps: true
+})
 
-// GET / get one profile by email
-// router.get('/login',getProfileByEmail)
-// POST 
-router.post('/login',getProfileByEmail)
+profileSchema.pre("save", function (next) {
+  const user = this
 
-// GET / get all profile by id
-router.get('/:id',getProfileById )
+  if (this.isModified("password",) || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        return next(saltError)
+      } else {
+        bcrypt.hash(user.password, salt, function(hashError, hash) {
+          if (hashError) {
+            return next(hashError)
+          }
 
-// POST / create profile
-router.post('/', createProfile)
+          user.password = hash
+          next()
+        })
+      }
+    })
+  } else {
+    return next()
+  }
+})
 
-router.put('/:id', updateProfileById )
-// GET / get all profile by id
-router.delete('/:id', deleteProfileById  )
-// GET / get all profile by id
+
+profileSchema.methods.comparePassword = function(password, callback) {
+  bcrypt.compare(password, this.password, function(error, isMatch) {
+    if (error) {
+      return callback(error)
+    } else {
+      callback(null, isMatch)
+    }
+  })
+}
 
 
-module.exports = router
+
+module.exports = mongoose.model('Profile', profileSchema)
